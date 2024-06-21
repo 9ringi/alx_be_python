404c7cbb7bd5bbd7c8d9bca32de2282b8cd78fc7
reda <9ringic@gmail.com>
2024-06-21 21:18:04 +0100

    this is a task

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,13 @@
+#Assign specific values to two variables, number1 and number2.
+number1 = 10
+number2 = 5
+
+#Perform addition, subtraction, and multiplication on these numbers.
+difference = number1 - number2
+product = number1 * number2
+
+
+
+#Print the results of these operations in a human-readable format.
+print("difference of", number1, "and", number2, "is", difference)
+print("product of", number1, "and", number2, "is", product)
