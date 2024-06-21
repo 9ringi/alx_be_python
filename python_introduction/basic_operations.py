4c39ee0cec66d0a2a6ee0b9d9287dd47a124f173
reda <9ringic@gmail.com>
2024-06-21 23:19:09 +0100

    added sum

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -3,6 +3,7 @@ number1 = 10
 number2 = 5
 
 #Perform addition, subtraction, and multiplication on these numbers.
+sum = number1 + number2
 difference = number1 - number2
 product = number1 * number2
 
@@ -11,3 +12,4 @@ product = number1 * number2
 #Print the results of these operations in a human-readable format.
 print("difference of", number1, "and", number2, "is", difference)
 print("product of", number1, "and", number2, "is", product)
+print("sum of", number1, "and", number2, "is", sum)
