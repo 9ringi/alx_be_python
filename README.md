fccb468475b8820c9a12ac15ad74284979765c97
reda <9ringic@gmail.com>
2024-06-21 23:40:58 +0100

    edited the readme file

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # alx_be_python
 alx_be_python
+##basic opearion
+To practice basic arithmetic operations in Python by performing predefined calculations.
+###simple_interest
+ basic Python arithmetic operations and variable assignments to calculate the simple interest on a given principal amount, rate of interest, and time.
---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,6 @@
+principal = 1000 #the principal amount (initial investment)
+rate = 0.05 #the annual interest rate (as a decimal),
+time = 3 #the time the money is invested for in years.
+interest = principal * rate * time #Calculate the simple interest using the formula provided and store the result in a variable named interest
+
+print("The simple interest is:", interest) #Print the calculated interest in a format
